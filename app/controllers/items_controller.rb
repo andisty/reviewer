56b78fa4d71372f5d89c9975445c6265ac32e732
andisty <distefano.andrea7@gmail.com>
2016-07-19 11:42:25 +0200

    add controller methods

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,44 @@
 class ItemsController < ApplicationController
+
+  def index
+      @items = Item.all
+      render json: @items
+    end
+
+    def destroy
+      @item = Item.find(params[:id])
+      @item.destroy
+      head :no_content
+    end
+
+    def create
+      @item = Item.new(item_params)
+      if @item.save
+        render json: @item
+      else
+        render json: @item.errors
+      end
+    end
+
+    def update
+      @item = Item.find(params[:id])
+      if @item.update(item_params)
+        render json: @item
+      else
+        render json: @item.errors
+      end
+    end
+
+
+    private
+
+      def item_params
+        params.require(:item).permit(:name, :description)
+      end
+
+
+
+
+
+end
 end
